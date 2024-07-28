c7a88237146f71d41a462a59b0e4a8876860d0ab
dcowett <david@davlinalli.com>
2024-07-28 15:15:44 -0400

    Finish user updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_25_223127) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_28_102830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_25_223127) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,17 @@
 User.create!(name:  "Test User",
              email: "test@testuser.com",
              password:              "foobar55",
-             password_confirmation: "foobar55")
+             password_confirmation: "foobar55",
+             admin: true)
+
+
+# Generate a bunch of additional users.
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password)
+  end
